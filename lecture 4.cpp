c76d5a94648d7b3b5ee993b427f518f7f2466f42
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-10 12:48:33 +0530

    Update lecture 4.cpp

--- lecture 4.cpp
+++ lecture 4.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+    int a,b,c;
+    a = 3;
+    b = 5;
+    c = a+b;
+    cout<<"Stored in variable c: "<<c<<endl;
+    cout<<"Printed directly: "<<a+b<<endl;
+    return 0;
+}
