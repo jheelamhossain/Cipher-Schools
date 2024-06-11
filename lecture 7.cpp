705bd5763cbf519d028daf6f79a4144ec803115b
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-11 13:58:26 +0530

    Create lecture 7.cpp

---
+++ lecture 7.cpp
@@ -0,0 +1,11 @@
+In this lecture we have learned about arrays and it's significance. How it differs from an integer input.
+Also we have done the following code:  
+int main()
+{
+int a,b,c,d,e;
+cout<<"Give 5 integers as input"<<endl;
+cin>>a>>b>>c>>d>>e;
+cout<<"The integers in reverse order are: "<<a<<b<<c<<d<<e;
+return e;
+}
+
