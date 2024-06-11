8736110c18736f9883fc75ffca951bc199e92a42
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-11 12:40:50 +0530

    Create lecture 6.cpp

---
+++ lecture 6.cpp
@@ -0,0 +1,13 @@
+In this lecture we have done the following :
+a) We have learned about conditional statements using if-else and switch case.
+b) #include <iostream>
+using namespace std;
+int main(){
+	float a,b;
+	cin>>a>>b;
+	
+	if(a>0.01){
+		cout<<"The condition in if statement is correct";
+	}
+}
+c) 
