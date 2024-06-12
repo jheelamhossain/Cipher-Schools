4542b57878547008de3c7a38cb6ed389f79e2fd2
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-12 21:20:03 +0530

    Create lecture 9.cpp

---
+++ lecture 9.cpp
@@ -0,0 +1,19 @@
+In this lecture we have covered the topic of loops:
+Loops are used to perform the same operation multiple times.
+We have discussed how loops work.
+We have discussed types of loops
+The different types of loops are:-
+	a) while loop
+  b) for loop
+	c) do while loop
+We have discussed how each loop works.
+We have also learned the meaning of initialisation i.e. what is happening at the beginning of the loop.This happens in loop that has not started yet.
+Also, this incomlete code was done: 
+#include <iostream>
+using namespace std;
+int main(){
+	int i;
+	for(i=0;){
+		cout<<i<<" ";
+	}
+}
