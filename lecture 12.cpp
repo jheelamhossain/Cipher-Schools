dfb6b6b482a88877ae65354ccd7c24c258053de3
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-13 17:53:48 +0530

    Update lecture 12.cpp

--- lecture 12.cpp
+++ lecture 12.cpp
@@ -1,1 +1,63 @@
 In this lecture we have completed the do while loop.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int password;
+	
+	cout<<"Enter the password: "<<endl;
+	cin>>password;
+	
+	do
+	{
+		cin>>password;
+	}
+	while(password<999999);
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int password;
+	
+	
+	
+	while(1)
+	{
+		cout<<"chocolates ";
+	}
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int password;
+	int count=0;
+	
+	
+	while(1)
+	{
+		cout<<"chocolates ";
+		count++;
+		
+		if(count>100) break;
+	}
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count=0;
+	int i;
+	
+	for(i=0; ;i++)
+	{
+		cout<<i<<" ";
+		if(i>100) break;
+	}
+	
+	return 0;
+}
