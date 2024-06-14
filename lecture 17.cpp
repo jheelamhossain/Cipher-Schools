d23c33bdcc9fe8fadd55df4f4c41bf799ae9b2d2
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-14 17:29:11 +0530

    Create lecture 17.cpp

---
+++ lecture 17.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+void PrintHi()
+{
+	cout<<"HI"<<endl;
+	
+	return ;
+}
+int main()
+{
+	PrintHi();
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+void PrintHi()
+{
+	cout<<"HI"<<endl;
+	
+	return ;
+}
+int sum(int a,int b)
+{
+	int c;
+	
+	c=a+b;
+	
+	return c;
+}
+int main()
+{
+	PrintHi();
+	
+	int a,b;
+	
+	a=4;
+	b=6;
+	
+	int d;
+	d=sum(a,b);
+	
+	cout<<"The sum is equal to "<<d;
+}
+
+#include <iostream>
+using namespace std;
+void PrintHi()
+{
+	cout<<"HI"<<endl;
+	
+	return ;
+}
+int sum(int a,int b)
+{
+	int c;
+	
+	c=a+b;
+	
+	return c;
+}
+int main()
+{
+	PrintHi();
+	
+	int a,b;
+	int c;
+	
+	a=4;
+	b=6;
+	c=75;
+	
+	int d;
+	d=sum(a,b);
+	
+	cout<<"The value of c is"<<c;
+}
+
+#include <iostream>
+using namespace std;
+void PrintHi()
+{
+	cout<<"HI"<<endl;
+	
+	return ;
+}
+int sum(int a,int b)
+{
+	int c;
+	
+	c=a+b;
+	a=500;
+	
+	cout<<"The value of c is "<<c;
+	
+	return c;
+}
+int main()
+{
+	PrintHi();
+	
+	int a,b;
+	int c;
+	
+	a=4;
+	b=6;
+	c=75;
+	
+	int d;
+	d=sum(a,b);
+	
+	cout<<"The value of c is "<<c;
+	
+	return c;
+}
+
