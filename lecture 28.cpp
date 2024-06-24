3f8d5c2ba2c30a0c336229ece6128a4582266f14
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-06-24 22:21:36 +0530

    Update lecture 28.cpp

--- lecture 28.cpp
+++ lecture 28.cpp
@@ -1,4 +1,9 @@
-
+Access Specifiers help to control visibility of data members and methods of a
+class
+The private access specifier can be used to hide information about the
+implementation detals ot the data members and methods
+Got Set rncthods are dotned to provide an intertacc tc use and access the
+Cata members
 #include <iostream>
 #include <string>
 using namespace std;
@@ -71,5 +76,61 @@ int main()
 	
 	return 0;
 }
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+	public:
+	
+	string name;
+	int attendance;
+	float total_marks;
+	
+	void calculate_percentage()
+	{
+		cout<<total_marks<<"%";
+		
+	}
+};
+class client
+{
+	private:
+	int credit_number;
+	int cvv;
+	string password;
+	public:
+	
+	int user_name;
+	string name;
+	
+	void set_credit_card_number(int number)
+	{
+		credit_number=number;
+	}
+	void get_creditcardnumber()
+	{
+		cout<<"The credit card number is "<<credit_number;
+		return credit_number;
+	}
+	void set_allthestuffatonce(int number,int name,int pass)
+	{
+		credit_number=num;
+		name=name;
+		password=pass;
+	}
+};
+int main()
+{
+	
+}
+
+
+
+
 
 
