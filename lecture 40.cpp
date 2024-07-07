08744dca2ad8f429d045617837fc7987c6397fdc
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-07-07 17:40:37 +0530

    Create lecture 40.cpp

---
+++ lecture 40.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"input an array of elements"<<endl;
+	
+	int i;
+	int a[6];
+	for(j=1;j<6;j++)
+	{
+	
+	for(i=0;i<6;i++)
+	{
+		cin>>a[i];
+	}
+	int key;
+	key=a[j];
+	i=j-1;
+	
+	while()
+	{
+		a[i+1]=a[i];
+		i--;
+	}
+	a[i+1]=key;
+}
+for(i=0;i<6;i++) cout<<a[i];
+}
