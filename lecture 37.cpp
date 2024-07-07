32ca328470ae94058dea93dae8c68614ddc6cfee
Jheelam Hossain <jheelam.12221424@lpu.in>
2024-07-07 16:40:00 +0530

    Create lecture 37.cpp

---
+++ lecture 37.cpp
@@ -0,0 +1,7 @@
+Selection sort is a sorting algorithm that helps us sort an array in a specific manner.
+Sorting is important in:
+Online Marketplaces
+Music Playlist
+Rankings and Leaderboards
+What happened during corona
+Databases and Search Engines
